7d679dac9996adf5ce6819032ad5cd1b0b3d13c7
Nels <nelsfichera@gmail.com>
2021-09-29 16:33:53 -0400

    j method

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
 '''creates list of hot post titles'''
 import requests
-import time
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], count=0, after=''):
     '''does the thing'''
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = headers = {'User-Agent': 'Python/1.0(Holberton Project)'}
-    request = requests.get(url, headers=headers, allow_redirects=False,
-                           params=payload)
-    payload = {'after': after}
-    try:
-        for post in request.json()['data']['children']:
-            hot_list.append(post['data']['title'])
-    except Exception:
+    base = 'https://www.reddit.com/'
+    endpoint = 'r/{}/hot.json'.format(subreddit)
+    query_string = '?show="all"&limit=100&after={}&count={}'.format(
+        after, count)
+    url = base + endpoint + query_string
+    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 2)'}
+    response = requests.get(url, headers=headers)
+    if not response.ok:
+        if len(hot_list) == 0:
+            return None
+        else:
+            return hot_list
+
+    data = response.json()['data']
+    for post in data['children']:
+        hot_list.append(post['data']['title'])
+    after = data['after']
+    dist = data['dist']
+    if (after):
+        recurse(subreddit, hot_list, count + dist, after)
+
+    if len(hot_list) == 0:
         return None
-    after = request.json()['data']['after']
-    if after not in [None, 'None', 'NULL', 'null']:
-        return recurse(subreddit, hot_list, after)
     else:
         return hot_list
